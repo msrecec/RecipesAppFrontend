93bbe1146a79bffc206d464d3d4a56aa68c0b4d6
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-09 15:25:06 +0200

    added ui

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+import { ShoppingList } from 'src/app/model/shopping-list/shopping-list';
+import { ShoppingListPaginated } from 'src/app/model/shopping-list/shopping-list-paginated';
+import { ShoppingListService } from 'src/app/services/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  constructor() {}
+  shoppingListPaginated!: ShoppingListPaginated;
 
-  ngOnInit(): void {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.shoppingListService
+      .getShoppingListPaginated()
+      .subscribe((shoppingListPaginated) => {
+        this.shoppingListPaginated = shoppingListPaginated;
+        console.log(shoppingListPaginated);
+      });
+  }
+
+  getPage(pageEvent: PageEvent): void {
+    this.shoppingListService
+      .getShoppingListPaginated(pageEvent.pageIndex)
+      .subscribe((shoppingListPaginated) => {
+        this.shoppingListPaginated = shoppingListPaginated;
+        console.log(shoppingListPaginated);
+      });
+  }
+
+  onClick(shoppingList: ShoppingList) {
+    console.log(shoppingList);
+    this.router.navigate(['shopping-list', shoppingList.id]);
+  }
+
+  addNewShoppingList() {
+    this.router.navigate(['shopping-list', 'new']);
+  }
 }
