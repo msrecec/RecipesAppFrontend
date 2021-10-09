fa9742216a99d3c3e0986a98aae05392dd403614
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-09 10:37:00 +0200

    added save ingredient page

--- src/app/command/ingredient/ingredient-save-command.ts
+++ src/app/command/ingredient/ingredient-save-command.ts
@@ -1,6 +1,17 @@
-export interface IngredientSaveCommand {
+export class IngredientSaveCommand {
   name: string;
   description: string;
   rating: number;
   priceHrk: number;
+  constructor(
+    name: string,
+    description: string,
+    rating: number,
+    priceHrk: number
+  ) {
+    this.name = name;
+    this.description = description;
+    this.rating = rating;
+    this.priceHrk = priceHrk;
+  }
 }
--- src/app/components/ingredient/new-ingredient/new-ingredient.component.ts
+++ src/app/components/ingredient/new-ingredient/new-ingredient.component.ts
@@ -1,15 +1,52 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { IngredientSaveCommand } from 'src/app/command/ingredient/ingredient-save-command';
+import { Ingredient } from 'src/app/model/ingredient/ingredient';
+import { IngredientService } from 'src/app/services/ingredient/ingredient.service';
 
 @Component({
   selector: 'app-new-ingredient',
   templateUrl: './new-ingredient.component.html',
-  styleUrls: ['./new-ingredient.component.css']
+  styleUrls: ['./new-ingredient.component.css'],
 })
 export class NewIngredientComponent implements OnInit {
+  name!: string;
+  description!: string;
+  rating!: number;
+  priceHrk!: number;
 
-  constructor() { }
+  constructor(
+    private _location: Location,
+    private ingredientService: IngredientService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  goBack() {
+    this._location.back();
   }
 
+  save() {
+    this.ingredientService
+      .postIngredient(
+        new IngredientSaveCommand(
+          this.name,
+          this.description,
+          this.rating,
+          this.priceHrk
+        )
+      )
+      .pipe(
+        tap((_) => console.log(`Creating ingredient with name: ${this.name}`)),
+        catchError(
+          this.ingredientService.handleError<Ingredient>(
+            `error while deleting ingredient with name: ${this.name}`
+          )
+        )
+      )
+      .subscribe(() => {
+        this.goBack();
+      });
+  }
 }
